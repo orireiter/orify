b69d7522dfdc7e6bb79ab222ccf25a0e89d07802
ori <ori.reiter@gmail.com>
2021-08-23 22:55:40 +0300

    added auth context to navigate

--- components/Login.js
+++ components/Login.js
@@ -1,34 +1,9 @@
 import React from "react";
-import { Alert, View, Text, Pressable, Linking } from "react-native";
+import { View, Text, Pressable, Linking } from "react-native";
 
-import CONF from "../conf";
-import { storeData } from "../utils/storageUtils";
-import {
-  getSpotifyRegisterUrl,
-  fetchSpotifyTokens,
-} from "../utils/spotifyUtils";
-
-const handleUrlCallback = async ({ url }) => {
-  let urlSplittedArray = url.split(`${CONF.SPOTIFY_REDIRECT_URI}/?code=`);
-  let codeStr = urlSplittedArray[1];
-  if (!codeStr) {
-    Alert.alert("Error", "Something went wrong when returning from Spotify");
-    return;
-  }
-
-  fetchSpotifyTokens(codeStr)
-    .then((spotifyAccountDetails) =>
-      storeData("spotifyAccount", spotifyAccountDetails)
-    )
-    .then(() => Linking.removeEventListener("url", handleUrlCallback))
-    .catch((error) => Alert.alert("Error", error));
-
-  // todo if success, redirect to homepage, otherwise alert error.
-};
+import { getSpotifyRegisterUrl } from "../utils/spotifyUtils";
 
 export default function Login() {
-  Linking.addEventListener("url", handleUrlCallback);
-
   const spotifyRegisterUrl = getSpotifyRegisterUrl();
 
   return (
